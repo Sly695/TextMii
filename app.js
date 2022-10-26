9bbb5b494bcd4d46a9551120e30d7d36fdc7401d
S7Y <sandaraly1@gmail.com>
2022-10-26 12:09:40 +0200

    build file

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(path.join(__dirname, 'textmii/public')));
+app.use(express.static(path.join(__dirname, 'textmii/build')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
