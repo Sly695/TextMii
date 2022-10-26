bd2527452766818d88396f0353ca721c9cc8c334
S7Y <sandaraly1@gmail.com>
2022-10-26 12:04:32 +0200

    fix deploy

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(path.join(__dirname, 'textmii/build')));
+app.use(express.static(path.join(__dirname, 'textmii/public')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
